7128be0e5fd7bf4693bdedd13c7f63d850fb6296
zqw10594884 <390544296@qq.com>
2016-03-07 17:00:14 +0800

    销售软件 进行中

--- src/com/zqw/bean/SaleOrderGoods.java
+++ src/com/zqw/bean/SaleOrderGoods.java
@@ -1,93 +1,42 @@
 package com.zqw.bean;
 
-import java.util.Date;
-
-
-
 public class SaleOrderGoods {
 	private int id;
 	private String serialNumber;
 	private double sellingPrice;
 	private double purchasePrice;
 	private double number;
-	private String curtainShop;
+	private String saleCustomer;
 	private int orderId;
-	private int owner;
 	private SaleOrderLst order;
-	private Date date;
 	private String remark;
-	
 
-	public SaleOrderGoods(String serialNumber, double sellingPrice,
-			double purchasePrice, double number, String curtainShop, int owner,String remark) {
+	public SaleOrderGoods() {
 		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSerialNumber() {
+		return serialNumber;
+	}
+
+	public void setSerialNumber(String serialNumber) {
 		this.serialNumber = serialNumber;
+	}
+
+	public double getSellingPrice() {
+		return sellingPrice;
+	}
+
+	public void setSellingPrice(double sellingPrice) {
 		this.sellingPrice = sellingPrice;
-		this.purchasePrice = purchasePrice;
-		this.number = number;
-		this.curtainShop = curtainShop;
-		this.owner = owner;
-		this.remark = remark;
-	}
-	
-	
-
-	public SaleOrderGoods(String serialNumber, double sellingPrice,
-			double purchasePrice, double number,Date date) {
-		super();
-		this.serialNumber = serialNumber;
-		this.sellingPrice = sellingPrice;
-		this.purchasePrice = purchasePrice;
-		this.number = number;
-		this.date = date;
-	}
-	
-	public SaleOrderGoods(String serialNumber, double sellingPrice,
-			double purchasePrice, double number) {
-		super();
-		this.serialNumber = serialNumber;
-		this.sellingPrice = sellingPrice;
-		this.purchasePrice = purchasePrice;
-		this.number = number;
-	}
-
-	public SaleOrderGoods(String serialNumber, double sellingPrice,
-			double purchasePrice, double number,int orderId) {
-		super();
-		this.serialNumber = serialNumber;
-		this.sellingPrice = sellingPrice;
-		this.purchasePrice = purchasePrice;
-		this.number = number;
-		this.orderId = orderId;
-	}
-
-
-
-	public String getRemark() {
-		return remark;
-	}
-
-
-
-	public void setRemark(String remark) {
-		this.remark = remark;
-	}
-
-
-
-	public Date getDate() {
-		return date;
-	}
-
-	public void setDate(Date date) {
-		this.date = date;
-	}
-	public int getOwner() {
-		return owner;
-	}
-
-	public void setOwner(int owner) {
-		this.owner = owner;
 	}
 
 	public double getPurchasePrice() {
@@ -96,6 +45,22 @@ public class SaleOrderGoods {
 
 	public void setPurchasePrice(double purchasePrice) {
 		this.purchasePrice = purchasePrice;
+	}
+
+	public double getNumber() {
+		return number;
+	}
+
+	public void setNumber(double number) {
+		this.number = number;
+	}
+
+	public String getSaleCustomer() {
+		return saleCustomer;
+	}
+
+	public void setSaleCustomer(String saleCustomer) {
+		this.saleCustomer = saleCustomer;
 	}
 
 	public int getOrderId() {
@@ -114,47 +79,12 @@ public class SaleOrderGoods {
 		this.order = order;
 	}
 
-	public String getSerialNumber() {
-		return serialNumber;
+	public String getRemark() {
+		return remark;
 	}
 
-	public void setSerialNumber(String serialNumber) {
-		this.serialNumber = serialNumber;
+	public void setRemark(String remark) {
+		this.remark = remark;
 	}
 
-	public SaleOrderGoods() {
-		super();
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public double getSellingPrice() {
-		return sellingPrice;
-	}
-
-	public void setSellingPrice(double sellingPrice) {
-		this.sellingPrice = sellingPrice;
-	}
-
-	public double getNumber() {
-		return number;
-	}
-
-	public void setNumber(double number) {
-		this.number = number;
-	}
-
-	public String getCurtainShop() {
-		return curtainShop;
-	}
-
-	public void setCurtainShop(String curtainShop) {
-		this.curtainShop = curtainShop;
-	}
 }
--- src/com/zqw/bean/SaleOrderLst.java
+++ src/com/zqw/bean/SaleOrderLst.java
@@ -1,6 +1,5 @@
 package com.zqw.bean;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -14,95 +13,4 @@ public class SaleOrderLst {
 	private int arrears;
 	private int orderState;
 
-	public SaleOrderLst() {
-		super();
-	}
-
-	public String getUntreatedPerson() {
-		return untreatedPerson;
-	}
-
-	public void setUntreatedPerson(String untreatedPerson) {
-		this.untreatedPerson = untreatedPerson;
-	}
-
-	public Date getSubmitTime() {
-		return submitTime;
-	}
-
-	public void setSubmitTime(Date submitTime) {
-		this.submitTime = submitTime;
-	}
-
-	public int getOrderState() {
-		return orderState;
-	}
-
-	/**
-	 * 
-	 * @param printState
-	 *            0 完成 1出库 2备货中 3提交
-	 */
-	public void setOrderState(int printState) {
-		this.orderState = printState;
-	}
-
-	public int getArrears() {
-		return arrears;
-	}
-
-	public void setArrears(int arrears) {
-		this.arrears = arrears;
-	}
-
-	public String getSaleCustomer() {
-		return saleCustomer;
-	}
-
-	public void setSaleCustomer(String saleCustomer) {
-		this.saleCustomer = saleCustomer;
-	}
-
-	public Date getDate() {
-		return date;
-	}
-
-	public String getSimpleDate() {
-		SimpleDateFormat sdf = new SimpleDateFormat("MM-dd");
-		return sdf.format(date);
-	}
-
-	public void setDate(Date date) {
-		this.date = date;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public List<SaleOrderGoods> getGoodsLst() {
-		return goodsLst;
-	}
-
-	public void setGoodsLst(List<SaleOrderGoods> goodsLst) {
-		this.goodsLst = goodsLst;
-	}
-
-	public String getOrderStateToString() {
-		String state = "";
-		if (0 == orderState) {
-			// do nothing
-		} else if (1 == orderState) {
-			state = "出库";
-		} else if (2 == orderState) {
-			state = "备货中";
-		} else if (3 == orderState) {
-			state = "提交";
-		}
-		return state;
-	}
 }
